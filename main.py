f1c55a06ebe71d6c3fbb20b3af541efd5b037635
Charl3s-T4n <104561880+Charl3s-T4n@users.noreply.github.com>
2022-04-29 17:51:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,19 @@
 #Method 1: Using for else with break statement 
 def prime_checker(number):
   if number > 1:
-    for num in range(2,number):
-      if number % num == 0:
+    for num in range(2,number):               # Exclude 1 and number 
+      if number % num == 0:                   # If the number can be evenly divided by a number between 1 and the number itself: not prime
         print("It's not a prime number.")
-        break
-    else:
+        break                                # Allows me to exit out of 'for' loop when 'if' condition is True: Hence only print that statement once 
+    else:                                  # Same indent as 'for' loop: 'else' will be executed only if a 'break' statement not encountered in 'for' loop
       print("It's a prime number.")
-  else:
+  else:                                         # Numbers that are <1 are not prime 
     print("It's not a prime number.")
 
     
 #Method 2: Using flag variable 
 def prime_checker(number):
-  is_prime = True
+  is_prime = True                   # Create flag variable 
   for num in range(2,number):
     if number % num ==0:
       is_prime = False
